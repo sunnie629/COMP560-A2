456acb570c0f9a40bbde2424370d8ed3f1129c43
Sunnie Kwak <sunnie@live.unc.edu>
2020-04-15 17:29:36 -0400

    setup2

--- modelbased.py
+++ modelbased.py
@@ -1,4 +1,5 @@
 # COMP560 A2 - MODEL BASED LEARNING // Sunnie Kwak
+
 import fileinput
 import random
 
@@ -10,10 +11,10 @@ state = ""
 action = ""
 actions = {}
 
+# file reading & set up of states dictionary
 for x in file_:
     word = x.split('/') 
     states[word[0]] = {}
-
 file_ = fileinput.input()
 for x in file_:
     word = x.split('/') 
@@ -34,18 +35,25 @@ for x in file_:
         action = word[1]
         states[word[0]].update({word[1]: {} })
         states[word[0]][word[1]].update({word[2] : float(word[3].rstrip())})
+
 state = "Fairway"
 
+#learning process
+# each time record starting posit, action took, resulting state
 while state != "In":
     a = random.choice(list(states[state].keys())) # choose random action to take
-    arr = []
     
+    arr = [] # temp arr used to select result state
     for x in states[state][a].keys(): # give weight to probabilities
         num = states[state][a][x] * 100
         for i in range(int(num)):
             arr.append(x)
-    state = random.choice(arr)
-    break
+    newstate = random.choice(arr) # choose state based on probabilities 
+
+    # transition probability : s, a, s'
+    print(a)
+    print(state)
+    
     #(list(states[state][a].keys()))
     
     
--- modelfree.py
+++ modelfree.py
@@ -1,1 +1,4 @@
 # COMP560 A2 - MODEL FREE LEARNING // Sunnie Kwak
+
+
+# record each time starting posit, action took, and # of strokes to finish hole
