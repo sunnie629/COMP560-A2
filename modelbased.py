4efaa7a47a28dd03de11c3d7914571afc077ab11
Sunnie Kwak <sunnie@live.unc.edu>
2020-04-21 20:49:59 -0400

    calculations for transition probability

--- modelbased.py
+++ modelbased.py
@@ -9,10 +9,13 @@ import fileinput
 import random
 import functions
 
-def explore(transitions, states, data):
+def explore(transition_prob, states, data):
     data = data 
-    iter = 1000 # number of iterations to record frequency
+    iter = 500 # number of iterations to record frequency; higher - more accurate
     counter = 0
+    reward = 1 # 
+    transitions = functions.transition_table_init()
+
     # each time record starting posit, action took, resulting state
     for i in range(iter):
         state = random.choice(states) 
@@ -25,20 +28,41 @@ def explore(transitions, states, data):
                 for i in range(int(num)):
                     arr.append(x)
             newstate = random.choice(arr) # choose state based on probabilities
-            transitions[state][a].update({newstate : transitions[state][a][newstate] + 1}) # increase count of s,a,s' event
-
+            
+            transitions[state][a].update({newstate : transitions[state][a][newstate] + 1}) # increase frequency of s,a,s'
             # transition probability : s, a, s'
             # print(state + ' ' + a + ' ' + newstate)
             state = newstate
             counter = counter + 1 # total number of runs
+    
+    # converting frequencies to transition probabilities
+    for s in transitions.keys():
+        for ac in transitions[s].keys():
+            reltotal = 0
+            for x in transitions[s][ac].keys():
+                reltotal = reltotal + transitions[s][ac][x]
+            if reltotal != 0:
+                for x in transitions[s][ac].keys():
+                    transition_prob[s][ac][x] = transitions[s][ac][x]/reltotal
     print(transitions)
+    print(transition_prob)
     print(counter)
+
+  # TODO: exploitation
+def exploit():
+    pass
 
 def __main__():
     data = functions.accept_input()  # initialize the dictionary with values from the .txt input
     states = functions.get_states(data)  # states is a list
-    transitions = functions.transition_table_init() # dict that will store counts of each s,a,s' occurence 
-    explore(transitions, states, data)
+    transition_prob = functions.transition_table_init() # dict that will store the transition prob P(s'|s,a)
+    
+    # TODO: Add epsilon and discount value functionality
+  
+    discountval = 0 # 0 - immediate reward / 1 - later reward
+    epsilon = 0 # explore rate  
+    
+    explore(transition_prob, states, data)
     
 
 if __name__ == '__main__':
